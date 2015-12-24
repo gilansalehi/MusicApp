f49cbeeb1f7e863b7415af41aed137f72051f238
App Academy Student <appacademy@Apps-Mac-mini-77.local>
2015-12-24 15:45:21 -0500

    Add Notes, full functionality.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,13 +2,20 @@ class NotesController < ApplicationController
 
   def new
     @track = Track.find(params[:id])
-    @note = @tracks.notes.new
+    @note = @track.notes.new
 
     render track_url(note_params[:track_id])
   end
 
   def create
     @note = Note.new(note_params)
+    if @note.user_id.nil?
+      @note.user_id = current_user.id
+    end
+    if @note.track_id.nil?
+      @note.track_id = params[:id]
+    end
+
     if @note.save
       redirect_to track_url(note_params[:track_id])
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ActiveRecord::Base
 
   has_many :notes
 
-
   # BASIC AUTHENTICATION
   def self.find_by_credentials(email, password)
     user = User.find_by_email(email)
