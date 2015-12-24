258e5e091b0a26792df7697e9dbfb5f4750fd9bd
App Academy Student <appacademy@Apps-Mac-mini-77.local>
2015-12-24 10:57:18 -0500

    Write UsersController.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+  def new
+    render :new
+  end
+
+  def show
+    render :show
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in!(@user)
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
